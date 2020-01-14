83ed2127bab229f1d6ca5047fb26513e093065af
tangxiao <1036747101@qq.com>
2020-01-14 09:18:13 +0800

    基础页面和各种依赖

--- www/handlers.py
+++ www/handlers.py
@@ -1,4 +1,9 @@
 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Michael Liao'
+
 ' url handlers '
 
 import re, time, json, logging, hashlib, base64, asyncio
@@ -8,15 +13,14 @@ from coroweb import get, post
 from models import User, Comment, Blog, next_id
 
 @get('/')
-async def index(request):
-    users = await User.findAll()
+def index(request):
+    summary = '摘要'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
     return {
-        '__template__': 'test.html',
-        'users': users
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
-# @asyncio.coroutine
-# @get('/')
-# def index(request): 
-#     # logging.info('查看属性' % User)
-#     users = yield from User.findAll() 
-#     return { 'template': 'test.html', 'users': users }
